8606adf44516ed2c83cb4befef0d6b8602f5537d
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2025-05-18 15:08:35 +0300

    feat: add message store

---
+++ message_store.py
@@ -0,0 +1,14 @@
+class MessageStore:
+    def __init__(self):
+        self.messages = []
+
+    def add_message(self, message):
+        self.messages.append(message)
+
+    def get_messages(self):
+        return self.messages
+
+    def clear_messages(self):
+        self.messages = []
+        print("Messages cleared.")
+        return True
